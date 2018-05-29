7ca0ce947bec19674c11fef2b7a8003cf4210cb3
Stavros Ereifej <stavros.ereifej@trade.io>
2018-05-29 17:55:47 +0300

    Removed fixed ip

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
     // Use special CloudFlare Headers to get real ip of user
     $ip = $_SERVER['REMOTE_ADDR'] = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'];
 
-    $ip = '31.13.218.192';
-
     $record = $reader->country($ip);
 
     $country_isoCode = $record->country->isoCode; // US
