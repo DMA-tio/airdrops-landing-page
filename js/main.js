ee7c88f3e50ee83a0cb4105e7a654125bdd3ec50
dmarkitanis <dmarkitanis@gmail.com>
2018-06-29 16:13:42 +0300

    Json Object finish - id's remaining on the buttons + content for the ICOs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 $( document ).ready(function() {
-    $("button").click(function() {
+    $("button.Open").click(function() {
         let card_id = $(this).attr('id');
         let card_id_formatted = card_id.split('-more-info').join('');
-        $('#card-title').text(JSONobject[card_id_formatted][card_id_formatted+'_title']);
+			
+        $('#card-title').html(JSONobject[card_id_formatted]['title']);
+			
+				$('#card-description').html(JSONobject[card_id_formatted]['text']);
+			
+				$('#website-link').html(JSONobject[card_id_formatted]['website_link']);
+				$('#website-link').attr('href', JSONobject[card_id_formatted]['website_link']);
+			
+				$('#country-restrictions').html(JSONobject[card_id_formatted]['country_restrictions']);
+			
+				$('#token-symbol').html(JSONobject[card_id_formatted]['token_symbol']);
+			
+				$('#accepted-currency').html(JSONobject[card_id_formatted]['accepted_currency']);
+			
+				$('#current-price').html(JSONobject[card_id_formatted]['current_price']);
+			
+				$('#airdrop-lp-link').attr('href', "https://airdrops.trade.io/" + JSONobject[card_id_formatted]['airdrop_lp_link']);
     });
 });
--- js/overlay_text.js
+++ js/overlay_text.js
@@ -1,10 +1,17 @@
 const JSONobject = {
     "card1": {
-        "card1_title": "Card 1 Title111111",
-        "card1_text": "Card 1 Text111111"
+				"image": "/images/airdrop_logos/xchangrate-logo.png",
+        "title": "2,000,000 TOKENS AVAILABLE FOR AIRDROP",
+        "text": "XchangeRate aims to provide a platform with an array of essential, powerful and valuable trading tools using augmented intelligence.<br><br> Successful day trading requires making decisions based on several market movements. Gathering the relevant information quickly and effortlessly requires advanced metric intelligence and automation.<br><br> XchangeRate provides all of that and more in a blend that makes it easy and quick to start reaping the profits and safely too. We proudly introduce our product to the World.",
+				"website_link": "https://tokensale.xchangerate.io/en/",
+				"country_restrictions": "USA, China, Singapore",
+				"token_symbol": "XRR",
+				"accepted_currency": "ETH, BTC",
+				"current_price": "1 ETH = 10,000 XRR",
+				"airdrop_lp_link": "xchangerate"
     },
     "card2": {
-        "card2_title": "Card 2 Title222222",
-        "card2_text": "Card 2 Text22222"
+        "title": "Card 2 Title222222",
+        "text": "Card 2 Text22222"
     },
 };
