e892439b5175f2c0ff210bd1c68cf4d37f708cf5
Stavros Ereifej <stavros.ereifej@trade.io>
2018-07-03 15:47:43 +0300

    Added airdrops portal

---
+++ airdrops-portal/js/overlay_text.js
@@ -0,0 +1,17 @@
+const JSONobject = {
+    "card1": {
+				"image": "/images/airdrop_logos/xchangrate-logo.png",
+        "title": "2,000,000 TOKENS AVAILABLE FOR AIRDROP",
+        "text": "XchangeRate aims to provide a platform with an array of essential, powerful and valuable trading tools using augmented intelligence.<br><br> Successful day trading requires making decisions based on several market movements. Gathering the relevant information quickly and effortlessly requires advanced metric intelligence and automation.<br><br> XchangeRate provides all of that and more in a blend that makes it easy and quick to start reaping the profits and safely too. We proudly introduce our product to the World.",
+				"website_link": "https://tokensale.xchangerate.io/en/",
+				"country_restrictions": "USA, China, Singapore",
+				"token_symbol": "XRR",
+				"accepted_currency": "ETH, BTC",
+				"current_price": "1 ETH = 10,000 XRR",
+				"airdrop_lp_link": "xchangerate"
+    },
+    "card2": {
+        "title": "Card 2 Title222222",
+        "text": "Card 2 Text22222"
+    },
+};
